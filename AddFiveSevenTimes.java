a7080d93c1ad30fec8ef05e5cb475a73b51879b8
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-07-22 09:22:40 +0900

    AddFiveSevenTimes

---
+++ AddFiveSevenTimes.java
@@ -0,0 +1,14 @@
+public class AddFiveSevenTimes {
+
+  public static void main(String[] args) {
+
+    int sum = 0;
+    for(int i = 0; i < 7; i++) {
+      sum += 5;
+    }
+
+    System.out.println(sum);
+
+  }
+
+}
