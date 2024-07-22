6c9256010f0d7972e49976f20eea2897547f2c16
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-07-22 09:25:47 +0900

    ArrayExample

---
+++ ArrayExample.java
@@ -0,0 +1,21 @@
+public class ArrayExample {
+
+  public static void main(String[] args) {
+
+    int a = 2;
+    int b = 3;
+    int c = 5;
+    int d = 7;
+    int e = 11;
+
+    int[] array = new int[]{a, b, c, d, e};
+
+    int result1 = array[1];
+    int result3 = array[3];
+
+    System.out.println(result1 + result3);
+
+
+  }
+
+}
