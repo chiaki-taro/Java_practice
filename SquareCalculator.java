86a122d7ebbffcd0c061ce05833d74164bb78aa6
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-07-22 09:28:58 +0900

    SquareCalculator

---
+++ SquareCalculator.java
@@ -0,0 +1,16 @@
+public class SquareCalculator {
+
+  public static void main(String[] args) {
+
+    int number = 5;
+    int result = calculateSquare(number);
+    System.out.println(result);
+
+  }
+
+  public static int calculateSquare(int x) {
+    return x * x;
+
+  }
+
+}
