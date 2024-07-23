de5464b8ba2c0c750164f5d0e867b7ba01d35b14
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-07-24 06:02:56 +0900

    addNum

---
+++ addNum.java
@@ -0,0 +1,17 @@
+public class addNum {
+
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 10;
+
+    num1 = num1 * 2;
+    num2 += 5;
+    num1 -= num2;
+    num1++;
+    num2--;
+
+    System.out.println("Final values- num1:" + num1 + " num2:" + num2);
+
+  }
+  
+}
