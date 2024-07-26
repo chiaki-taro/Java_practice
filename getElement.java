659ed0d45d211a6c14697fd2ad7545b0d37b2504
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-07-27 07:12:49 +0900

    getElement

---
+++ getElement.java
@@ -0,0 +1,7 @@
+public class getElement {
+  public static void main(String[] args) {
+    String[] fruits = {"apple", "banana", "cherry", "date", "elderberry"};
+    System.out.println(fruits[2]);
+  }
+
+}
