5b8f76501708b17c540116dd89a9352ea3143ac6
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-08-02 06:19:55 +0900

    ConvertDoDecimal

---
+++ ConvertDoDecimal.java
@@ -0,0 +1,11 @@
+public class ConvertDoDecimal {
+
+  public static void main(String[] args) {
+    
+    int a = 5;
+    int b = 2;
+    double c = ((double)a / b);
+    System.out.println(c);
+  }
+  
+}
