82404ad363a12800f67d34d790b79a9f5911a1b7
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-08-02 06:33:57 +0900

    ConvertToInteger

---
+++ ConvertToInteger.java
@@ -0,0 +1,8 @@
+public class ConvertToInteger {
+  public static void main(String[] args) {
+    String strNum = "123";
+    int num = Integer.parseInt(strNum);
+    System.out.println(num);
+  }
+  
+}
